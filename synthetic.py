9031daf21fb4e28c46b0e4e43158783210b5d817
zhongzi <zhongzi@reed.edu>
2018-01-20 12:34:22 -0500

    put recovered signal of Isomap and TSNE onto a single plot

--- synthetic.py
+++ synthetic.py
@@ -48,37 +48,48 @@ if __name__ == "__main__":
     XS = make_manifold(X)
 
     # Decrease dimension via Isomap/t-SNE
-    # model = TSNE(n_components=2)
-    model = Isomap(n_neighbors=15, n_components=2, eigen_solver='dense')
-    X_recovered = model.fit_transform(XS)
+    model_tsne = TSNE(n_components=2)
+    model_isomap = Isomap(n_neighbors=15, n_components=2, eigen_solver='dense')
+    X_tsne = model_tsne.fit_transform(XS)
+    X_isomap = model_isomap.fit_transform(XS)
 
     # Rotate axes via ICA
     ica = FastICA(random_state=rng)
-    S_ica_ = ica.fit(X_recovered).transform(X_recovered)  # Estimate the sources
+    # Estimate the sources
+    S_isomap = ica.fit(X_isomap).transform(X_isomap)
+    S_tsne = ica.fit(X_tsne).transform(X_tsne)
 
     # #############################################################################
 
     fig = plt.figure()
-    fig.add_subplot(2, 3, 1)
+    fig.add_subplot(3, 3, 1)
     plot_samples(S / S.std())
     plt.title('True Independent Sources')
 
-    fig.add_subplot(2, 3, 2)
+    fig.add_subplot(3, 3, 2)
     plot_samples(X / np.std(X))
     plt.title('Observations')
 
-    ax = fig.add_subplot(2, 3, 3, projection='3d')
+    ax = fig.add_subplot(3, 3, 3, projection='3d')
     # colorize = dict(c=X[:, 0], cmap=plt.cm.get_cmap('rainbow', 5))
     ax.scatter3D(XS[:, 0], XS[:, 1], XS[:, 2])
     plt.title('Manifold')
 
-    fig.add_subplot(2, 3, 4)
-    plot_samples(X_recovered / np.std(X_recovered))
+    fig.add_subplot(3, 3, 4)
+    plot_samples(X_isomap / np.std(X_isomap))
     plt.title('Recovered(Isomap)')
 
-    fig.add_subplot(2, 3, 5)
-    plot_samples(S_ica_ / S_ica_.std(axis=0))
+    fig.add_subplot(3, 3, 5)
+    plot_samples(S_isomap / S_isomap.std(axis=0))
     plt.title('Recovered(Isomap+ICA)')
+
+    fig.add_subplot(3, 3, 6)
+    plot_samples(X_tsne / np.std(X_tsne))
+    plt.title('Recovered(TSNE)')
+
+    fig.add_subplot(3, 3, 7)
+    plot_samples(S_tsne / S_tsne.std(axis=0))
+    plt.title('Recovered(TSNE+ICA)')
 
     plt.subplots_adjust(0.09, 0.04, 0.94, 0.94, 0.26, 0.36)
     plt.show()

c8ca663ad5cb0806040b329156ab007c7bac1a41
zhongzi <zhongzi@reed.edu>
2018-02-06 15:59:22 -0500

    modify code and image for tSNE with different perplexity and distance between clusters

--- tSNE.py
+++ tSNE.py
@@ -1,20 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-
-from sklearn.decomposition import FastICA
-from sklearn.manifold import Isomap, TSNE
+from sklearn.manifold import TSNE
 
 
 # Plot results in 2D
-def plot_samples(S, axis_list=None):
-    colorize = dict(c=S[:, 0], cmap=plt.cm.get_cmap('rainbow', 2))
-    plt.scatter(S[:, 0], S[:, 1], **colorize, s=2, marker='o', zorder=10,
+def plot_samples(S):
+    # colorize = dict(c=S[:, 0], cmap=plt.cm.get_cmap('rainbow', 2))
+    plt.scatter(S[:, 0], S[:, 1], s=2, marker='o', zorder=10,
                 alpha=0.5)
-    plt.hlines(0, -3, 3)
-    plt.vlines(0, -3, 3)
-    plt.xlim(-3, 3)
-    plt.ylim(-3, 3)
+    plt.hlines(0, -2, 2)
+    plt.vlines(0, -2, 2)
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
     plt.xlabel('x')
     plt.ylabel('y')
 
@@ -30,22 +27,21 @@ def rotate(X, angle):
 def make_manifold(X):
     x = X[:, 0]
     y = X[:, 1]
-    z = (x/2+y/2)**2
-    return np.vstack((x, y, z)).T
+    z = np.ones((x.shape))  # (x/2+y/2)**2
+    XS = np.vstack((x, y, z)).T
+    return XS
 
 
-def tSNE_exp(num, distance):
+def tSNE_exp(num, distance, perp):
     d = distance/2
 
     # Generate sample data
-    rng = np.random.RandomState(42)
-
-    S1x = rng.uniform(-2, -d, size=(num, 1))
-    S1y = rng.uniform(-2, 2, size=(num, 1))
+    S1x = np.random.uniform(-0.5-d, -d, size=(num, 1))
+    S1y = np.random.uniform(-1, 1, size=(num, 1))
     S1 = np.hstack((S1x, S1y))
 
-    S2x = rng.uniform(d, 2, size=(num, 1))
-    S2y = rng.uniform(-2, 2, size=(num, 1))
+    S2x = np.random.uniform(d, 0.5+d, size=(num, 1))
+    S2y = np.random.uniform(-1, 1, size=(num, 1))
     S2 = np.hstack((S2x, S2y))
 
     S = np.vstack((S1, S2))
@@ -54,48 +50,33 @@ def tSNE_exp(num, distance):
     XS = make_manifold(S)
 
     # Decrease dimension via t-SNE
-    model_tsne = TSNE(n_components=2, perplexity=10.0)
+    model_tsne = TSNE(n_components=2, perplexity=perp)
     X_tsne = model_tsne.fit_transform(XS)
 
     return S, XS, X_tsne
 
 if __name__ == "__main__":
 
-    num1 = 80
-    num2 = 1000
+    # Specify number of points in each cluster, distance
+    # between the two cluster and perplexity of each run
 
-    S, XS, X_tsne = tSNE_exp(num1, 0.3)
-    S2, XS2, X_tsne2 = tSNE_exp(num2, 0.3)
-
-    # #############################################################################
+    num = 500
+    d = [0.2, 0.05, 0.0001]
+    perp = [10, 30]
+    col = len(d)
+    row = len(perp)
 
     fig = plt.figure()
 
-    fig.add_subplot(2, 3, 1)
-    plot_samples(S / S.std())
-    plt.title('True Independent Sources '+str(num1))
+    for i in range(col):
+        for j in range(row):
+            S, XS, X_tsne = tSNE_exp(num, d[i], perp[j])
+            if j == 0:
+                fig.add_subplot(row+1, col, i+1)
+                plot_samples(S)
+                plt.title('a='+str(d[i]))
+            fig.add_subplot(row+1, col, i+1+(j+1)*col)
+            plot_samples(X_tsne / np.std(X_tsne))
+            plt.title('sigma='+str(perp[j]))
 
-    ax = fig.add_subplot(2, 3, 2, projection='3d')
-    colorize = dict(c=S[:, 0], cmap=plt.cm.get_cmap('rainbow', 2))
-    ax.scatter3D(XS[:, 0], XS[:, 1], XS[:, 2], **colorize)
-    plt.title('Manifold '+str(num1))
-
-    fig.add_subplot(2, 3, 3)
-    plot_samples(X_tsne / np.std(X_tsne))
-    plt.title('Recovered(TSNE) '+str(num1))
-
-    fig.add_subplot(2, 3, 4)
-    plot_samples(S2 / S2.std())
-    plt.title('True Independent Sources '+str(num2))
-
-    ax2 = fig.add_subplot(2, 3, 5, projection='3d')
-    colorize = dict(c=S2[:, 0], cmap=plt.cm.get_cmap('rainbow', 2))
-    ax2.scatter3D(XS2[:, 0], XS2[:, 1], XS2[:, 2], **colorize)
-    plt.title('Manifold '+str(num2))
-
-    fig.add_subplot(2, 3, 6)
-    plot_samples(X_tsne2 / np.std(X_tsne2))
-    plt.title('Recovered(TSNE) '+str(num2))
-
-    plt.subplots_adjust(0.09, 0.04, 0.94, 0.94, 0.26, 0.36)
     plt.show()

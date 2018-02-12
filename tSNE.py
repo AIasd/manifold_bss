7fb74a14ecc8907d0c2c08e21b89819e12f0f658
zhongzi <zhongzi@reed.edu>
2018-02-11 22:35:19 -0500

    modify tSNE to support different arrangements and add some graphs

--- tSNE.py
+++ tSNE.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 from sklearn.manifold import TSNE
 
 
 # Plot results in 2D
-def plot_samples(S):
-    # colorize = dict(c=S[:, 0], cmap=plt.cm.get_cmap('rainbow', 2))
-    plt.scatter(S[:, 0], S[:, 1], s=2, marker='o', zorder=10,
+def plot_samples(S, tot_num):
+    colorize = dict(c=S[:, 0], cmap=plt.cm.get_cmap('rainbow', tot_num))
+    plt.scatter(S[:, 0], S[:, 1], **colorize, s=1, marker='o', zorder=10,
                 alpha=0.5)
     plt.hlines(0, -2, 2)
     plt.vlines(0, -2, 2)
@@ -14,6 +15,11 @@ def plot_samples(S):
     plt.ylim(-2, 2)
     plt.xlabel('x')
     plt.ylabel('y')
+
+
+def plot_samples_3d(XS, ax, tot_num):
+    colorize = dict(c=XS[:, 0], cmap=plt.cm.get_cmap('rainbow', tot_num))
+    ax.scatter3D(XS[:, 0], XS[:, 1], XS[:, 2], **colorize, s=2)
 
 
 def rotate(X, angle):
@@ -32,51 +38,57 @@ def make_manifold(X):
     return XS
 
 
-def tSNE_exp(num, distance, perp):
-    d = distance/2
+# size e.g. (4, 3) will generate 4 horizontal rectangular blocks
+# at three different heights
+def tSNE_exp(size, num, d, perp):
+    h_num = size[0]
+    v_num = size[1]
+
+    S_list = []
 
     # Generate sample data
-    S1x = np.random.uniform(-0.5-d, -d, size=(num, 1))
-    S1y = np.random.uniform(-1, 1, size=(num, 1))
-    S1 = np.hstack((S1x, S1y))
-
-    S2x = np.random.uniform(d, 0.5+d, size=(num, 1))
-    S2y = np.random.uniform(-1, 1, size=(num, 1))
-    S2 = np.hstack((S2x, S2y))
-
-    S = np.vstack((S1, S2))
-
-    # Create 2D manifold in 3D
-    XS = make_manifold(S)
+    for i in range(h_num):
+        for j in range(v_num):
+            Sx = np.random.uniform((1+d)*i, (1+d)*i+1, size=(num, 1))
+            Sy = np.random.uniform(-1, 1, size=(num, 1))
+            Sz = np.random.uniform((1+d)*j, (1+d)*j+1, size=(num, 1))
+            S1 = np.hstack((Sx, Sy, Sz))
+            S_list.append(S1)
+    XS = np.vstack(S_list)
 
     # Decrease dimension via t-SNE
     model_tsne = TSNE(n_components=2, perplexity=perp)
     X_tsne = model_tsne.fit_transform(XS)
 
-    return S, XS, X_tsne
+    return XS, X_tsne
 
 if __name__ == "__main__":
 
     # Specify number of points in each cluster, distance
     # between the two cluster and perplexity of each run
 
-    num = 500
+    num = 700
     d = [0.2, 0.05, 0.0001]
     perp = [10, 30]
     col = len(d)
     row = len(perp)
 
+    h_num = 3
+    v_num = 3
+    tot_num = h_num*v_num
+    size = [h_num, v_num]
+
     fig = plt.figure()
 
     for i in range(col):
         for j in range(row):
-            S, XS, X_tsne = tSNE_exp(num, d[i], perp[j])
+            XS, X_tsne = tSNE_exp(size, num, d[i], perp[j])
             if j == 0:
-                fig.add_subplot(row+1, col, i+1)
-                plot_samples(S)
+                ax = fig.add_subplot(row+1, col, i+1, projection='3d')
+                plot_samples_3d(XS, ax, tot_num)
                 plt.title('a='+str(d[i]))
             fig.add_subplot(row+1, col, i+1+(j+1)*col)
-            plot_samples(X_tsne / np.std(X_tsne))
-            plt.title('sigma='+str(perp[j]))
+            plot_samples(X_tsne / np.std(X_tsne), tot_num)
+            plt.title('perplexity='+str(perp[j]))
 
     plt.show()
